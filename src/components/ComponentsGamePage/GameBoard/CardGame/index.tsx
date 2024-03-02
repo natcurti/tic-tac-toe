e277f52c32e55ae357e89ee517a9b7438231d40a
natcurti <naticurti01@gmail.com>
2024-03-02 16:21:59 -0300

    Apply hover effect on cards

--- src/components/ComponentsGamePage/GameBoard/CardGame/index.tsx
+++ src/components/ComponentsGamePage/GameBoard/CardGame/index.tsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import cross from "src/assets/img/x-blue.svg";
 import circle from "src/assets/img/circle-yellow.svg";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { WhoIsTurnContext } from "src/context/WhoIsTurnContext";
 
-const CardGameContainer = styled.div`
+const CardGameContainer = styled.div<{
+  $iconHover: string;
+  $isHovered: boolean;
+}>`
   width: 6rem;
   height: 6rem;
   background-color: var(--medium-gray);
@@ -15,6 +18,15 @@ const CardGameContainer = styled.div`
 
   &:hover {
     cursor: pointer;
+    background-image: ${(props) =>
+      props.$isHovered && props.$iconHover === "cross"
+        ? 'url("src/assets/img/cross-outlined.svg")'
+        : props.$isHovered && props.$iconHover === "circle"
+        ? 'url("src/assets/img/oval-outlined.svg")'
+        : null};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 2.9rem;
   }
 
   img {
@@ -30,17 +42,23 @@ const CardGameContainer = styled.div`
       width: 4rem;
       height: 4rem;
     }
+
+    &:hover {
+      background-size: 3.9rem;
+    }
   }
 
   @media (min-width: 768px) {
     width: 7.5rem;
-    height: 8rem;
+    height: 7.5rem;
   }
 `;
 
 export const CardGame = () => {
   const context = useContext(WhoIsTurnContext);
   const [cardIcon, setCardIcon] = useState("");
+  const [iconHover, setIconHover] = useState("cross");
+  const [isHovered, setIsHovered] = useState(true);
 
   const changeTurnAndAddIcon = () => {
     if (context?.turn === "cross") {
@@ -50,10 +68,23 @@ export const CardGame = () => {
       setCardIcon(circle);
       context?.setTurn("cross");
     }
+    setIsHovered(false);
   };
 
+  useEffect(() => {
+    if (context?.turn === "cross") {
+      setIconHover("cross");
+    } else {
+      setIconHover("circle");
+    }
+  }, [context?.turn]);
+
   return (
-    <CardGameContainer onClick={changeTurnAndAddIcon}>
+    <CardGameContainer
+      onClick={changeTurnAndAddIcon}
+      $iconHover={iconHover}
+      $isHovered={isHovered}
+    >
       {cardIcon !== "" ? <img src={cardIcon} /> : null}
     </CardGameContainer>
   );
